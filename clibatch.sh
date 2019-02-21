c0099c5d104c73bfd3a10684a76f5e84d23d27aa
Test Man <ttestman4@gmail.com>
2019-02-22 00:38:59 +0530

    settings files

---
+++ clibatch.sh
@@ -0,0 +1,9 @@
+npm run ng -- g module home --routing
+npm run ng -- g component home/mod1comp1 -m module1/module1.module
+# npm run ng -- g component module1/mod1comp2 -m module1/module1.module
+# npm run ng -- g component module1/mod1host -m module1/module1.module
+
+# npm run ng -- g module module2 --routing
+# npm run ng -- g component module2/mod2comp1 -m module2/module2.module
+# npm run ng -- g component module2/mod2comp2 -m module2/module2.module
+# npm run ng -- g component module2/mod2host -m module2/module2.module
